f0994c2afb6169a3597aca80cd66ab77dfbd88cb
HarryZ10 <hzhu20@georgefox.edu>
2024-03-18 20:51:51 -0700

    Clean up

--- src/components/Experiences/Experiences.js
+++ src/components/Experiences/Experiences.js
@@ -49,7 +49,7 @@ function a11yProps(index) {
 export default function Experiences() {
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
+  const handleChange = (_, newValue) => {
     setValue(newValue);
   };
 
